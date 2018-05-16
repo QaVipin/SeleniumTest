66ae2342031601fed663f3ea5e94d421dca2cf36
vipin rawat <vipin.rawat@outlook.com>
2018-05-16 13:10:33 +0530

    added driver

--- eBayDDUAutomation/src/main/java/com/eBay/Base/BaseTest.java
+++ eBayDDUAutomation/src/main/java/com/eBay/Base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.eBay.Base;
 
 import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,6 +11,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+
 
 public class BaseTest {
 
@@ -30,11 +32,9 @@ public class BaseTest {
 	
 	@BeforeTest
 	public static void setup(final ITestContext testContext){
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\vi016ra\\Downloads\\chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\vi016ra\\Downloads\\geckodriver.exe");
-		//driver=new ChromeDriver();
-		driver =new HtmlUnitDriver();
+		System.setProperty("webdriver.chrome.driver",BaseTest.class.getResource("/drivers/chromedriver.exe").toString());
 		
+		driver=new ChromeDriver();		
 		if(testContext.getName().contains("eBaySellerSignUP")){
 			driver.get(configReader.getEbay_developer_url());
 		}else{
